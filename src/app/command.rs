beadd80a7cccf20981a47118f91da4ad708c4aa7
maxmwang <orangemax888@gmail.com>
2022-11-16 05:03:46 -0800

    Play songs with another thread

--- src/app/command.rs
+++ src/app/command.rs
@@ -1,9 +1,16 @@
 use std::process::Command;
+use std::thread;
 
-pub fn play(id: &str) {
-    Command::new("mpv")
-        .arg(format!("https://www.youtube.com/watch?v={id}").as_str())
-        .arg("--no-video")
-        .output()
-        .expect("failed to execute process");
+pub fn play(id: &String) {
+    // ISSUES: multiple threads can be used -> multiple songs can be played at once
+
+    let id_copy = id.clone();
+
+    thread::spawn(move || {
+        Command::new("mpv")
+            .arg(format!("https://www.youtube.com/watch?v={id_copy}").as_str())
+            .arg("--no-video")
+            .output()
+            .expect("failed to execute process");
+    });
 }
